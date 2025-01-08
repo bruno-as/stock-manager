5cb26ad4af2ef0d12b8aa4827f5b727bbd92f57d
Bruno <bruno.drd@gmail.com>
2025-01-07 23:22:07 -0300

    feat: pom update and application.yaml

--- src/main/java/org/stockmanager/Main.java
+++ src/main/java/org/stockmanager/Main.java
@@ -1,5 +1,8 @@
 package org.stockmanager;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
     }
--- src/main/java/org/stockmanager/entity/Stock.java
+++ src/main/java/org/stockmanager/entity/Stock.java
@@ -1,10 +1,15 @@
 package org.stockmanager.entity;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
